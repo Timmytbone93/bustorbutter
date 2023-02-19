ac4ff3428df6ecd27d6393a9f1317a43779c0c43
Timmytbone93 <Timothyhunt1993@gmail.com>
2023-02-19 17:39:05 -0500

    new

--- client/src/views/Dashboard/Dashboard.jsx
+++ client/src/views/Dashboard/Dashboard.jsx
@@ -1,16 +1,19 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
+import UserContext from "../../context/UserContext/UserContext";
 
 import { Container } from "semantic-ui-react";
 
 function Dashboard() {
+  const [currentUser, setCurrentUser] = useContext(UserContext);
   return (
     <div className="Dashboard">
       <Container fluid>
         <Navbar />
+        {currentUser.authenticated && }
       </Container>
     </div>
   );
